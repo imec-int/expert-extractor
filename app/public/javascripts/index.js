f445431858d0d7c19c8904f84a09b10f56deae73
kaosbeat <kasper.jordaens@gmail.com>
2013-12-09 19:12:46 +0100

    commitje

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -1,7 +1,20 @@
 App = {
 	pageloaded: function() {
 		console.log("page loaded");
+		// $('#moreinfo').hide();
 	}
+
 };
 
 $(App.pageloaded);
+
+
+function moreinfo(panel) {
+
+	var hgt = $('.'+panel).height();
+	console.log(hgt);
+	$('#moreinfo').height(hgt);
+	$("#topexpertlist .profilepanel:not('."+panel+"')").hide();
+	$( "#moreinfo" ).appendTo( "#topexpertlist > row" );
+
+}
