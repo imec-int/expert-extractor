e4a3228d56abaad6f1a5bf759c44647daf7790a7
Sam Decrock <sam.decrock@gmail.com>
2013-12-10 11:11:08 +0100

    search button werkt al zie

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -2,9 +2,23 @@ App = {
 	pageloaded: function() {
 		console.log("page loaded");
 		// $('#moreinfo').hide();
+
+
+		$("#searchbtn").click(function (event){
+			var topic = $("#topicfield").val();
+			console.log("searchbtn clicked and searching for " + topic);
+
+			$.get('/api/searchtopic', {topic: topic}, function (data){
+				if(data.err) return console.log(data.err);
+				var experts = data;
+
+				console.log(experts); //hiermee de pagina populeren
+			});
+		});
 	}
 
 };
+
 
 $(App.pageloaded);
 
