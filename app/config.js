dee576132781c88b7301448bf02f114196126cfd
Matthias De Geyter <matthias.degeyter@ibbt.be>
2014-01-20 15:32:57 +0100

    updated gitignore and forced dummy file in

---
+++ app/config.js
@@ -0,0 +1,21 @@
+// get your keys here : https://dev.twitter.com/
+exports.twitter = {
+	consumerKey: 	'',
+	consumerSecret: ''
+}
+
+// https://textalytics.com/core/topics-info
+// get key here: https://textalytics.com/home
+exports.textalytics = {
+	key: ''
+}
+
+// not really needed for the current demo:
+exports.diffbot = {
+	token: ''
+}
+
+exports.algorithm = {
+	retweetsForTopicScore : 3,
+	tweetAboutTopicScore  : 1
+}
